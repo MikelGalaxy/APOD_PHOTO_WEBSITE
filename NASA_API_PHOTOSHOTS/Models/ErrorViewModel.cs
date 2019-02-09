8aff63e364241f1d2393adbac251ca42785c0788
Michał Boniakowski <michal.boniakowski@gmail.com>
2019-02-09 22:42:23 +0100

    Add project files.

---
+++ NASA_API_PHOTOSHOTS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NASA_API_PHOTOSHOTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
